365b15e58b92c7f8a8d1ce6e4b67c6e5a688d530
kayqueFranco <kayque.franco2018@gmail.com>
2025-01-27 17:16:40 -0300

    Iniciar o projeto

---
+++ app.js
@@ -0,0 +1,61 @@
+/**
+ * Simulador dos operadores lógicos AND- OR - NOT
+ * @author Kayque Francp
+ */
+
+let recive  //armazena o parâmetro(valor) recebido do html
+//as variáveis abaixo são parte para identificar 
+//se o interrupitor está ligado ou desligado(false = desligado| true = ligado)
+let sw1 = false
+let sw2 = false
+
+let broken = false
+
+
+function sw(recive) {
+    //  console.log(recive) //apoio a logica
+
+    // Logia para os interrupitores
+    if (recive === 1 && sw1 === false) {
+        document.getElementById('sw1').src = "img/swon.png"
+        sw1 = true
+    }
+    else if (recive === 1 && sw1 === true) {
+        document.getElementById('sw1').src = "img/swoff.png"
+        sw1 = false
+
+    }
+
+    else if (recive === 2 && sw2 === false) {
+        document.getElementById('sw2').src = "img/swon.png"
+        sw2 = true
+    }
+    else if (recive === 2 && sw2 === true) {
+        document.getElementById('sw2').src = "img/swoff.png"
+        sw2 = false
+    }
+    if (recive === 3) {
+        //A linha abaixo cria um objeto usado a classe Audio
+        let som = new Audio()
+        console.log(typeof (som))
+        som.src = "sound/glassbreaking.wav"
+        som.play()
+        //trocar a imagem (Manipulação de tom)
+        document.getElementById('lamp').src = "img/broken.jpg"
+        broken = true
+    }
+
+    // logica para o operador  AMd
+    if (broken !== true){
+    if (sw1 === true && sw2 === true) {
+        document.getElementById('lamp').src = "img/on.jpg"
+    }
+    else {
+        document.getElementById('lamp').src = "img/off.jpg"
+    }
+    }
+
+
+
+
+}
